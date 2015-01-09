f029eebd5b44d4614359db5da208dd2f0e4a291e
Jeff Thelen <jeff.thelen@gmail.com>
2015-01-08 22:23:19 -0600

    Add Amazon S3 for Paperclip uploads to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,17 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+# Required for Heroku
 #set to actual host name
 config.action_mailer.default_url_options = { :host => 'http://jjt-firstapp.herokuapp.com/' }
+
+# Sets Paperclip to upload images to Amazon S3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+
 end
