3cf5686d3ce87ccf80a972aae29aa80233dde7d4
Jeff Thelen <jeff.thelen@gmail.com>
2015-01-08 22:38:18 -0600

    Amazon S3 attempt 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,6 @@ config.paperclip_defaults = {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-
+  }  
+}
 end
